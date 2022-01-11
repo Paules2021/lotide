57e84a4bc45cae7c3183908d2eee6eb35125b933
Paules2021 <P.elishaee@gmail.com>
2022-01-11 03:22:19 +0000

    reverse

---
+++ reverse.js
@@ -0,0 +1,20 @@
+function reverse(str){
+  let newStrin = "";
+  for(let i=str.length - 1; i >= 0; i--){
+    newStrin += str[i];
+  } return newStrin;
+}
+
+//or
+
+console.log(reverse("hello"))
+
+function reverseString(str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
+};
+
+console.log(reverseString("hello"));
+
