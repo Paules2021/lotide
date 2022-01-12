35de384cdf0a57f6d4c832f1809d5c7c2b7c5b4c
Paules2021 <P.elishaee@gmail.com>
2022-01-12 18:51:10 +0000

    final

--- middle.js
+++ middle.js
@@ -13,3 +13,19 @@ const middle = function(arr){
 console.log(middle([1, 2, 3 , 4, 5, 6]))
 
 module.exports = middle;
+
+/*
+const middle = function(array) {
+  let middleArr = [];
+  if (array.length <= 2) {
+
+  } else if (array.length % 2 !== 0) {
+    middleArr.push(array[Math.floor(array.length / 2)]);
+  } else {
+    middleArr.push(array[(array.length / 2) - 1], array[array.length / 2]);
+  }
+  // Log and/or return the array of the middle element(s)
+  console.log(middleArr);
+  return middleArr;
+};
+*/
