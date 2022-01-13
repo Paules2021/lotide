29360a079e66aa28e164ccfc983fbae208a8eea5
Paules2021 <P.elishaee@gmail.com>
2022-01-13 22:29:34 +0000

    final

---
+++ takeUntil.js
@@ -0,0 +1,70 @@
+
+// function to compare arrays
+const eqArrays = function(arr1, arr2) {
+  
+  //compare that length of both the arrays are same
+  if (arr1.length === arr2.length) {
+    //initialize index with 0 and assume both arrays are equal, so equal = true
+    let index = 0;
+    let equal = true;
+
+    //loop through length of an array elements
+    while (index < arr1.length) {
+      //if values of both array at current index is not same store false to equal and break out of loop
+      if (arr1[index] !== arr2[index]) {
+        equal = false;
+        break;
+      }
+      index++;
+    }
+    //return false or true
+    return equal;
+
+  } else {
+    //if array lengths of both array are not same then return false
+    return false;
+  }
+
+};
+
+//function to show assert arrays are equal or not
+const assertArraysEqual = function(actual, expected) {
+  //store emoji from decimal codes
+  const emojiPassed = String.fromCodePoint(128512);
+  const emojiFailed = String.fromCodePoint(128125);
+
+  //check if both arrays are equal or not and log appropriate result
+  const equalArrays = eqArrays(actual, expected);
+  if (equalArrays)
+    console.log(`${emojiPassed}${emojiPassed}${emojiPassed} Assertion Passed: [${actual}] === [${expected}]`);
+  else
+    console.log(`${emojiFailed}${emojiFailed}${emojiFailed} Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+//function to show array elements until callback fucntion return true
+const takeUntil = function(array, callback) {
+  //define final return element with empty array
+  const filteredArray = [];
+
+  //loop through array elements
+  for (let element of array) {
+    //make a call to callback function and if it returns true, break out of loop
+    if (callback(element))
+      break;
+    //store current element to final return element
+    filteredArray.push(element);
+  }
+
+  return filteredArray;
+};
+
+//TEST CODE
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
